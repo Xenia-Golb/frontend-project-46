0b7ed3f29a8069cfbd8b0b098ce45694e5794fda
Xenia Golbraykh <karzerin@gmail.com>
2025-12-15 19:31:48 +0300

    style: fix lint errors

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,41 +1,41 @@
-const _ = require('lodash')
+const _ = require("lodash");
 
 const stringify = (value) => {
   if (_.isObject(value) && !_.isArray(value)) {
-    return '[complex value]'
+    return "[complex value]";
   }
-  if (value === null) return 'null'
-  if (typeof value === 'string') return `'${value}'`
-  return String(value)
-}
+  if (value === null) return "null";
+  if (typeof value === "string") return `'${value}'`;
+  return String(value);
+};
 
 const buildPlainLines = (ast, path = []) => {
   return ast.flatMap((node) => {
-    const currentPath = [...path, node.key].join('.')
+    const currentPath = [...path, node.key].join(".");
 
     switch (node.type) {
-      case 'added':
+      case "added":
         return [
           `Property '${currentPath}' was added with value: ${stringify(
-            node.value,
+            node.value
           )}`,
-        ]
-      case 'removed':
-        return [`Property '${currentPath}' was removed`]
-      case 'changed':
+        ];
+      case "removed":
+        return [`Property '${currentPath}' was removed`];
+      case "changed":
         return [
           `Property '${currentPath}' was updated. From ${stringify(
-            node.value1,
+            node.value1
           )} to ${stringify(node.value2)}`,
-        ]
-      case 'nested':
-        return buildPlainLines(node.children, [...path, node.key])
+        ];
+      case "nested":
+        return buildPlainLines(node.children, [...path, node.key]);
       default:
-        return []
+        return [];
     }
-  })
-}
+  });
+};
 
-const plain = (ast) => buildPlainLines(ast).join('\n')
+const plain = (ast) => buildPlainLines(ast).join("\n");
 
-module.exports = plain
+module.exports = plain;
